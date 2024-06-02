4444cf404f820b011e76a55db1e0507cdfedd1ed
adzaninada <adzaninada@gmail.com>
2024-06-02 16:24:42 +0700

    home page, banner, pagination

--- client/klontong-shop/src/App.vue
+++ client/klontong-shop/src/App.vue
@@ -1,10 +1,94 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+// import TheWelcome from './components/TheWelcome.vue'
+import Navbar from './components/Navbar.vue'
+import { ref, computed } from 'vue'
+
+const currentPage = ref(1)
+const pageSize = ref(10)
+const totalItems = ref(100)
+
+const totalPages = computed(() => Math.ceil(totalItems.value / pageSize.value))
+
+const prevPage = () => {
+  if (currentPage.value > 1) {
+    currentPage.value--
+  }
+}
+
+const nextPage = () => {
+  if (currentPage.value < totalPages.value) {
+    currentPage.value++
+  }
+}
 </script>
 
 <template>
-  <header>
+  <Navbar />
+
+  <div class="pt-24 w-[95%] mx-auto justify-center items-center relative">
+    <img
+      src="./assets/imgs/klontong-banner.jpg"
+      alt="Banner"
+      class="h-[540px] mx-auto w-[1440px] object-cover"
+    />
+
+    <div class="absolute top-[40%] left-[42.5%] text-white text-5xl font-extrabold">
+      <p class="mt-2">Shop that</p>
+      <p class="mt-2">moves you</p>
+      <p class="mt-2">forward.</p>
+    </div>
+
+    <button
+      class="absolute top-[80%] left-[45%] w-36 rounded-xl h-12 border-black bg-white text-cyan-800 font-extrabold hover:bg-cyan-800 hover:text-white transition-all"
+    >
+      Learn more
+    </button>
+  </div>
+
+  <main>
+    <h3 class="text-center text-4xl font-bold mt-8">Our products</h3>
+
+    <!-- Card container (Reusable component) -->
+    <div class="w-[95%] mx-auto bg-gray-200 h-[32rem] mt-10">
+      <div class="flex gap-8 mb-8">
+        <div class="w-72 h-72 bg-gray-400"></div>
+        <div class="w-72 h-72 bg-gray-400"></div>
+        <div class="w-72 h-72 bg-gray-400"></div>
+        <div class="w-72 h-72 bg-gray-400"></div>
+        <div class="w-72 h-72 bg-gray-400"></div>
+      </div>
+      <div class="flex gap-8">
+        <div class="w-72 h-72 bg-gray-400"></div>
+        <div class="w-72 h-72 bg-gray-400"></div>
+        <div class="w-72 h-72 bg-gray-400"></div>
+        <div class="w-72 h-72 bg-gray-400"></div>
+        <div class="w-72 h-72 bg-gray-400"></div>
+      </div>
+
+      <!-- Pagination bar -->
+      <div class="flex w-full mx-auto mt-8 gap-2 justify-center items-center">
+        <button
+          :disabled="currentPage === 1"
+          @click="prevPage"
+          class="rounded-2xl bg-cyan-600 text-white w-20 h-10 mr-8"
+        >
+          Previous
+        </button>
+        <span>Page {{ currentPage }} of {{ totalPages }}</span>
+        <button
+          :disabled="currentPage === totalPages"
+          @click="nextPage"
+          class="rounded-2xl bg-cyan-600 text-white w-20 h-10 ml-8"
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  </main>
+
+  <!-- Vue elements as a reference (I just started learning) -->
+  <!-- <header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
@@ -14,34 +98,5 @@ import TheWelcome from './components/TheWelcome.vue'
 
   <main>
     <TheWelcome />
-  </main>
+  </main> -->
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
---
+++ client/klontong-shop/src/components/Navbar.vue
@@ -0,0 +1,13 @@
+<script setup></script>
+
+<template>
+  <nav class="fixed h-24 w-full bg-white z-10">
+    <div class="flex items-center w-full h-24 mb-12 border-t-4 border-cyan-950 border-b text-xl">
+      <ul class="flex justify-between w-full mx-24">
+        <li class="text-cyan-600 font-semibold">Menu</li>
+        <li class="text-cyan-800 font-extrabold text-2xl">Klontong</li>
+        <li class="text-cyan-600 font-semibold">Sign in</li>
+      </ul>
+    </div>
+  </nav>
+</template>
